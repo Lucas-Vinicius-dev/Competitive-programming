0a7ce460c493e1b7d45e3b1466ca6df746bdb462
Lucas-Vinicius-dev <cavalcanti.costa@academico.ifpb.edu.br>
2026-02-01 14:46:18 -0300

    GCD of Odd and Even Sums

---
+++ leetcode/3658.cpp
@@ -0,0 +1,13 @@
+#include "std_includes.h"
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+/* Average Performance
+    Runtime: 0ms | Beats 100.00%
+    Memory: 8.63MB | Beats 08.03%
+*/
